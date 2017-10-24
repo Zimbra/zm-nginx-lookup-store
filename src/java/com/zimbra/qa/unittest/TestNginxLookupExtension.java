29110a4b77730a13545d6dd29ea8471512a4199f
Gren Elliot <gren.elliot@synacor.com>
2017-10-24 19:16:40 +0100

    ZCS-3269:IMAP tests - accept IMAP daemon ports
    Allow either embedded or IMAP daemon ports to be returned from IMAP
    lookups for ports.

--- src/java/com/zimbra/qa/unittest/TestNginxLookupExtension.java
+++ src/java/com/zimbra/qa/unittest/TestNginxLookupExtension.java
@@ -18,9 +18,11 @@
 package com.zimbra.qa.unittest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,8 +31,11 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
+import com.google.common.base.Joiner;
 import com.zimbra.common.account.Key;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
@@ -46,7 +51,10 @@ import com.zimbra.cs.nginx.NginxLookupExtension;
  */
 public class TestNginxLookupExtension {
 
-    private static final String USER = "testnginxlookupextension-user1";
+    @Rule
+    public TestName testInfo = new TestName();
+
+    private static String USER;
     private Domain externalRouteDomain = null;
     private Account externalRouteAccount = null;
     private static String DEFAULT_DOMAIN = null;
@@ -63,6 +71,8 @@ public class TestNginxLookupExtension {
     private static final String POP3_SSL_PORT = "7995";
     private static final String IMAP_PORT     = "7143";
     private static final String IMAP_SSL_PORT = "7993";
+    private static final String IMAP_DAEMON_PORT     = "8143";
+    private static final String IMAP_DAEMON_SSL_PORT = "8993";
     private static final String HTTP_PORT     = LC.zimbra_mail_service_port.value();
 
     private enum AuthMethod {
@@ -173,19 +183,26 @@ public class TestNginxLookupExtension {
 
 
         void assertAuthStatusOK() {
-            assertEquals("OK", authStatus());
+            assertEquals("Auth Status", "OK", authStatus());
         }
 
         void assertAuthUser(String expected) {
-            assertEquals(expected, authUser());
+            assertEquals("Auth User", expected, authUser());
         }
 
         void assertAuthServer(String expected) {
-            assertEquals(expected, authServer());
+            assertEquals("Auth server", expected, authServer());
         }
 
         void assertAuthPort(String expected) {
-            assertEquals(expected, authPort());
+            assertEquals("Auth port", expected, authPort());
+        }
+
+        void assertAuthPort(String[] expected) {
+            String port = authPort();
+            assertTrue(String.format(
+                    "Auth port %s should be one of [%s']", port, Joiner.on(',').join(expected)),
+                    Arrays.asList(expected).contains(port));
         }
 
         void assertBasic(String expectedUser, String expectedServer, String expectedPort) {
@@ -193,6 +210,13 @@ public class TestNginxLookupExtension {
             assertAuthUser(expectedUser);
             assertAuthServer(expectedServer);
             assertAuthPort(expectedPort);
+        }
+
+        void assertBasic(String expectedUser, String expectedServer, String[] expectedPorts) {
+            assertAuthStatusOK();
+            assertAuthUser(expectedUser);
+            assertAuthServer(expectedServer);
+            assertAuthPort(expectedPorts);
         }
     }
 
@@ -297,16 +321,17 @@ public class TestNginxLookupExtension {
 
     @Before
     public void setUp() throws Exception {
-        tearDown();
         InetAddress IP = InetAddress.getLocalHost();
         MY_IP_ADDRESS = IP.getHostAddress();
         DEFAULT_DOMAIN = AccountTestUtil.getDomain();
+        USER = testInfo.getMethodName() + "1";
         QUSER = USER + "@" +DEFAULT_DOMAIN;
+        tearDown();
     }
 
     @After
     public void tearDown() throws Exception {
-        TestUtil.deleteAccount(USER);
+        TestUtil.deleteAccountIfExists(USER);
         deleteAccount(externalRouteAccount);
         deleteDomain(externalRouteDomain);
         externalRouteAccount = null;
@@ -318,7 +343,8 @@ public class TestNginxLookupExtension {
         TestUtil.createAccount(QUSER);
         LookupData lookupData = new LookupData(AuthMethod.plain, QUSER, PASSWORD, AuthProtocol.imap);
         RespHeaders respHdrs = senRequest(lookupData);
-        respHdrs.assertBasic(QUSER, MY_IP_ADDRESS, IMAP_PORT);
+        String[] ports = {IMAP_PORT, IMAP_DAEMON_PORT};
+        respHdrs.assertBasic(QUSER, MY_IP_ADDRESS, ports);
     }
 
     @Test
@@ -326,7 +352,8 @@ public class TestNginxLookupExtension {
         TestUtil.createAccount(QUSER);
         LookupData lookupData = new LookupData(AuthMethod.plain, QUSER, PASSWORD, AuthProtocol.imapssl);
         RespHeaders respHdrs = senRequest(lookupData);
-        respHdrs.assertBasic(QUSER, MY_IP_ADDRESS, IMAP_SSL_PORT);
+        String[] ports = {IMAP_SSL_PORT, IMAP_DAEMON_SSL_PORT};
+        respHdrs.assertBasic(QUSER, MY_IP_ADDRESS, ports);
     }
 
     @Test
